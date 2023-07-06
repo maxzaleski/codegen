41900b02dd7323cd361701966901cb1fd082e463
Maximilien Zaleski <maximilienzaleski@yahoo.com>
2023-07-06 18:48:31 +0100

    test: added slice_test.go

--- internal/utils/slice/slice_test.go
+++ internal/utils/slice/slice_test.go
@@ -17,3 +17,29 @@ func TestMap(t *testing.T) {
 		t.Errorf("Expected %v, but got %v", expected, result)
 	}
 }
+
+func TestFilter(t *testing.T) {
+	input := []int{1, 2, 3, 4, 5}
+	expected := []int{2, 4}
+
+	result := Filter(input, func(i int) bool {
+		return i%2 == 0
+	})
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Expected %v, but got %v", expected, result)
+	}
+}
+
+func TestMapKeys(t *testing.T) {
+	input := map[string]int{
+		"one":   1,
+		"two":   2,
+		"three": 3,
+	}
+	expected := []string{"one", "two", "three"}
+
+	result := MapKeys(input)
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Expected %v, but got %v", expected, result)
+	}
+}
